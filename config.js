ff20726fe369ce71518d9dc8a36eb7b8e21656f2
cgildea15 <gildeac@gmail.com>
2026-04-05 10:45:53 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// This file holds secret lab keys
+const firebaseConfig = {
+    apiKey: "YOUR_NEW_API_KEY_HERE",
+    authDomain: "winelab-c2f76.firebaseapp.com",
+    projectId: "winelab-c2f76",
+    storageBucket: "winelab-c2f76.firebasestorage.app",
+    messagingSenderId: "625162326304",
+    appId: "1:625162326304:web:e53604a064dca64d6d0745"
+};
