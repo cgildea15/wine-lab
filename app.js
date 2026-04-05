7bfecc9660389054c14ac6acded3c7c53479a668
cgildea15 <gildeac@gmail.com>
2026-04-05 10:51:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyA2GxSOi1McA7frSXuiq66igVrAqB7kPqo",
-    authDomain: "winelab-c2f76.firebaseapp.com",
-    projectId: "winelab-c2f76",
-    storageBucket: "winelab-c2f76.firebasestorage.app",
-    messagingSenderId: "625162326304",
-    appId: "1:625162326304:web:e53604a064dca64d6d0745"
-};
+// AI Wine Predictor - Main Logic
+// Note: firebaseConfig is loaded from the external config.js file for security.
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
