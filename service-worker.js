1e296d19dd3c68562bd84639f554b25456e9d94e
cgildea15 <gildeac@gmail.com>
2026-04-12 16:47:19 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,38 @@
-self.addEventListener('fetch', function(event) {
-  // This is a basic service worker to allow installation
+const CACHE_NAME = 'wine-lab-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/config.js',
+  '/manifest.json'
+];
+
+// Install: Cache the basic app shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate: Clean up old caches if you update the version
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: Serve from cache, then update from network (Stale-While-Revalidate)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
