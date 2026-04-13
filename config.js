494169b27d83ec96c760b8931b1da7742049059b
cgildea15 <gildeac@gmail.com>
2026-04-13 12:47:29 +0000

    final professional structure with public folder

--- config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyA2GxSOi1McA7frSXuiq66igVrAqB7kPqo",
-    authDomain: "winelab-c2f76.firebaseapp.com",
-    projectId: "winelab-c2f76",
-    storageBucket: "winelab-c2f76.firebasestorage.app",
-    messagingSenderId: "625162326304",
-    appId: "1:625162326304:web:e53604a064dca64d6d0745"
-};
